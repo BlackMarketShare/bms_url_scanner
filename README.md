9565dec8301f68df973c68cfd60af151ac655180
Sairam Koripelly <skkoripe@amazon.com>
2023-11-17 23:12:22 +0530

    Added feature to write the classified links to respective files

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Sairam Koripelly
 
 ### Usage
 1. cd into the root directory (`bms_url_scanner`)
-1. Add the required links to be scanned in the `src/resources/test_links`
-2. Run the command `node --trace-warnings src/url_scanner.js src/resources/test_links 10` 
+2. Add the required links to be scanned in the `src/resources/test_links`
+3. Run the command `node --trace-warnings src/url_scanner.js src/resources/test_links 10` 
 (here 10 refers to the url batch count to be scanned at a given point)
+
+### Output
+1. All the deadSites are written to **src/output/deadSites.txt**
+2. All the marketPlaceSitesToBeConfigured sites are written to **src/output/marketPlaceSitesToBeConfigured.txt**
+3. All the sitesTobeCheckedManually sites are written to **src/output/sitesTobeCheckedManually.txt**
--- src/url_scanner.js
+++ src/url_scanner.js
@@ -1,6 +1,7 @@
 const {readFileSync} = require('fs');
 const {MarketplaceEvaluator} = require('./MarketplaceEvaluator');
 require('events').EventEmitter.defaultMaxListeners = 0;
+const fs = require('fs');
 
 // Defines the concurrent urls count to be scanned
 
@@ -21,24 +22,17 @@ function getMarketplaceInfo(url) {
     return MarketplaceEvaluator['DEFAULT'];
 }
 
-function printUrlList(urlList) {
-    urlList.forEach(url => console.log(url));
-    console.log();
-}
-
-async function classifyURL(url) {
-    console.log(`Evaluating ${url}`)
+async function classifyURL(url, evaluatedCount) {
+    console.log(`${evaluatedCount}. Evaluating ${url}`)
     let marketPlace = getMarketplaceInfo(url);
     let isURLRemoved = await marketPlace.evaluate(url);
     if (isURLRemoved) {
         console.log(`URL is dead - ${url}`);
         deadSites.push(url);
-    }
-    else if (marketPlace.marketplaceQuery === 'default') {
+    } else if (marketPlace.marketplaceQuery === 'default') {
         console.log(`URL marketplace to be configured - ${url}`);
         marketPlaceSitesToBeConfigured.push(url);
-    }
-    else {
+    } else {
         console.log(`URL to be checked manually - ${url}`);
         sitesTobeCheckedManually.push(url);
     }
@@ -48,26 +42,59 @@ async function classifyURLs(filePath, concurrentLimit) {
     const file = readFileSync(filePath, 'utf8');
     const urls = file.split(/\r?\n/);
     const promiseList = [];
-    for (const url of urls) {
+    let evaluatedCount = 1;
+    let urlsLength = urls.length;
 
+    for (const url of urls) {
         if (promiseList.length == concurrentLimit) {
             await Promise.all(promiseList);
             promiseList.length = 0;
         }
-        promiseList.push(classifyURL(url));
+
+        promiseList.push(classifyURL(url, evaluatedCount));
+        evaluatedCount++;
+
+        // Write data to files every 25 links evaluated
+        if (evaluatedCount % 5 === 0 || evaluatedCount == urlsLength + 1) {
+            await Promise.all(promiseList);
+            console.log("Writing the links to respective files...")
+            // Write dead sites to a file
+            fs.appendFileSync('src/output/deadSites.txt',
+                deadSites.join('\n'));
+            if (deadSites.length != 0) {
+                fs.appendFileSync('src/output/deadSites.txt', '\n');
+            }
+
+            // Write sites to be checked manually to a file
+            fs.appendFileSync('src/output/sitesTobeCheckedManually.txt',
+                sitesTobeCheckedManually.join('\n'));
+            if (sitesTobeCheckedManually.length != 0) {
+                fs.appendFileSync('src/output/sitesTobeCheckedManually.txt', '\n');
+            }
+
+            // Write marketplaces to be configured to a file
+            fs.appendFileSync('src/output/marketPlaceSitesToBeConfigured.txt',
+                marketPlaceSitesToBeConfigured.join('\n'));
+            if (marketPlaceSitesToBeConfigured.length != 0) {
+                fs.appendFileSync('src/output/marketPlaceSitesToBeConfigured.txt', '\n');
+            }
+
+            // Clear the arrays after writing
+            deadSites.length = 0;
+            sitesTobeCheckedManually.length = 0;
+            marketPlaceSitesToBeConfigured.length = 0;
+            promiseList.length = 0;
+        }
     }
-    await Promise.all(promiseList);
-    console.log('Dead Sites :');
-    printUrlList(deadSites);
-    console.log('To be checked Manually :');
-    printUrlList(sitesTobeCheckedManually);
-    console.log('Marketplaces to be configured :');
-    printUrlList(marketPlaceSitesToBeConfigured);
 }
 
 // fetching file from command line
 const filePath = process.argv[2];
-const concurrentLimit = process.argv[3];
+let concurrentLimit = process.argv[3];
+if (concurrentLimit == null) {
+    concurrentLimit = 5;
+    console.log(`Setting url batch size scan count as ${concurrentLimit}`);
+}
 classifyURLs(filePath, concurrentLimit)
 
 
